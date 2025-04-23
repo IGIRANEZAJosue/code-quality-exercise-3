61c1d285636fe0cb2d16a168c008bdff99f136eb
Josue Igiraneza <88510074+IGIRANEZAJosue@users.noreply.github.com>
2025-04-23 11:56:50 +0200

    refactor: change loop limit to the square root of that number.
    You only need to check up to the square root of num because if a number has a divisor larger than its square root, the corresponding smaller divisor has already been found.

--- checkPrimeNumber.js
+++ checkPrimeNumber.js
@@ -1,5 +1,6 @@
 const checkPrimeNumber = (num) => {
-   for (let i = 2; i < num; i++) {
+   if (num < 2) return false 
+   for (let i = 2; i < Math.sqrt(num); i++) {
       if (num % i === 0) return false;
    }
    return true;
